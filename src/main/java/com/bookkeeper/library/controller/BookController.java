7bd95977e5b02bc219d435800a27c58ae98e7431
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-28 10:59:25 -0500

    get book by id

--- src/main/java/com/bookkeeper/library/controller/BookController.java
+++ src/main/java/com/bookkeeper/library/controller/BookController.java
@@ -11,14 +11,20 @@ public class BookController {
 
 
     // http://localhost:9092/api/books/
-    @PostMapping("/books/")
+    @PostMapping("/books")
     public Book createBook(@RequestBody Book body) {
         return body;
     }
 
     // http://localhost:9092/api/books/
-    @GetMapping(path = "/books/")
+    @GetMapping(path = "/books")
     public String getBooks() {
         return "get all books";
+    }
+
+    // http://localhost:9092/api/books/{bookId}
+    @GetMapping(path = "/books/{bookId}")
+    public String getBook(@PathVariable Long bookId) {
+        return "getting the book with the id of " + bookId;
     }
 }
