b482ad42b1f50d0c5cd77df3e5b052e71d51f492
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-28 15:00:57 -0500

    auto wire bookrepo

--- src/main/java/com/bookkeeper/library/service/BookService.java
+++ src/main/java/com/bookkeeper/library/service/BookService.java
@@ -1,5 +1,15 @@
 package com.bookkeeper.library.service;
 
-public class BookService {
+import com.bookkeeper.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class BookService {
+private BookRepository bookRepository;
+
+    @Autowired
+    public void  setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 }
