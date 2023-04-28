8373ff2e6ccb1c81d2401f5a927ebaf08b66d1bd
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-28 15:09:43 -0500

    post book service

--- src/main/java/com/bookkeeper/library/controller/BookController.java
+++ src/main/java/com/bookkeeper/library/controller/BookController.java
@@ -4,6 +4,7 @@ import com.bookkeeper.library.exception.InformationExistException;
 import com.bookkeeper.library.exception.InformationNotFoundException;
 import com.bookkeeper.library.model.Book;
 import com.bookkeeper.library.repository.BookRepository;
+import com.bookkeeper.library.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,24 +16,24 @@ import java.util.Optional;
 public class BookController {
 
     private BookRepository bookRepository;
+    private BookService bookService;
 
     @Autowired
     public void setBookRepository(BookRepository bookRepository){
         this.bookRepository = bookRepository;
     }
 
+    @Autowired
+    public void setBookService(BookService bookService){
+        this.bookService = bookService;
+    }
 
 
     // http://localhost:9092/api/books/
     @PostMapping("/books")
     public Book createBook(@RequestBody Book bookObject) {
 
-        Book book = bookRepository.findByName(bookObject.getName());
-        if (book != null) {
-            throw new InformationExistException("A book with name " + book.getName() + " already exists");
-        } else {
-            return bookRepository.save(bookObject);
-        }
+        return bookService.createBook(bookObject);
     }
 
     // http://localhost:9092/api/books/
--- src/main/java/com/bookkeeper/library/service/BookService.java
+++ src/main/java/com/bookkeeper/library/service/BookService.java
@@ -1,8 +1,11 @@
 package com.bookkeeper.library.service;
 
+import com.bookkeeper.library.exception.InformationExistException;
+import com.bookkeeper.library.model.Book;
 import com.bookkeeper.library.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class BookService {
@@ -11,5 +14,15 @@ private BookRepository bookRepository;
     @Autowired
     public void  setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    public Book createBook(@RequestBody Book bookObject) {
+
+        Book book = bookRepository.findByName(bookObject.getName());
+        if (book != null) {
+            throw new InformationExistException("A book with name " + book.getName() + " already exists");
+        } else {
+            return bookRepository.save(bookObject);
+        }
     }
 }
