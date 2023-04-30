5516eb5dfb18bf08b68b458226f509bb6ce44707
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-30 14:51:58 -0500

    book model doc

--- src/main/java/com/bookkeeper/library/model/Book.java
+++ src/main/java/com/bookkeeper/library/model/Book.java
@@ -5,11 +5,11 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "books")
+@Table(name = "books")// specifies the table name for this entity in the database
 public class Book {
     @Id
     @Column
-    @GeneratedValue(strategy = GenerationType.IDENTITY)//primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// specifies that the primary key is generated automatically
     private Long id;
     @Column
     private String name;
@@ -17,44 +17,76 @@ public class Book {
     private String description;
 
     @ManyToOne
-    @JoinColumn(name = "author_id")
-    @JsonIgnore
+    @JoinColumn(name = "author_id") // specifies the foreign key column name in the database
+    @JsonIgnore // specifies that the author field should be ignored when serializing to JSON
     private Author author;
 
     @ManyToOne
-    @JoinColumn(name = "genre_id")
+    @JoinColumn(name = "genre_id")// specifies the foreign key column name in the database
     private Genre genre;
 
     public Book() {
     }
-
+    /**
+     * Constructor for Book
+     *
+     * @param id the book ID
+     * @param name the book name
+     * @param description the book description
+     */
     public Book(Long id, String name, String description) {
         this.id = id;
         this.name = name;
         this.description = description;
     }
-    // foreignkey connecting to books oneToMany
+    // Getter and setter methods for all fields
+
+    /**
+     * Getter for the id field
+     *
+     * @return the id field value
+     */
 
     public Long getId() {
         return id;
     }
-
+    /**
+     * Setter for the id field
+     *
+     * @param id the value to set for the id field
+     */
     public void setId(Long id) {
         this.id = id;
     }
-
+    /**
+     * Getter for the name field
+     *
+     * @return the name field value
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * Setter for the name field
+     *
+     * @param name the value to set for the name field
+     */
     public void setName(String name) {
         this.name = name;
     }
-
+    /**
+     * Getter for the description field
+     *
+     * @return the description field value
+     */
     public String getDescription() {
         return description;
     }
-
+    /**
+     * Setter for the description field
+     *
+     * @param description the value to set for the description field
+     */
     public void setDescription(String description) {
         this.description = description;
     }
@@ -67,19 +99,35 @@ public class Book {
                 ", description='" + description + '\'' +
                 '}';
     }
-
+    /**
+     * Getter for the author field
+     *
+     * @return the author field value
+     */
     public Author getAuthor() {
         return author;
     }
-
+    /**
+     * Setter for the author field
+     *
+     * @param author the value to set for the author field
+     */
     public void setAuthor(Author author) {
         this.author = author;
     }
-
+    /**
+     * Getter for the genre field
+     *
+     * @return the genre field value
+     */
     public Genre getGenre() {
         return genre;
     }
-
+    /**
+     * Setter for the genre field
+     *
+     * @param genre the value to set for the genre field
+     */
     public void setGenre(Genre genre) {
         this.genre = genre;
     }
