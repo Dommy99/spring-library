7614131bd27f9ec756b3fe4b9feca322f49b1351
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-29 23:40:29 -0500

    gen JWT tokens

--- src/main/java/com/bookkeeper/library/service/JWTUtils.java
+++ src/main/java/com/bookkeeper/library/service/JWTUtils.java
@@ -1,8 +1,12 @@
 package com.bookkeeper.library.service;
 
+import com.bookkeeper.library.security.MyAuthorDetails;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.logging.Logger;
 
 @Service
@@ -15,4 +19,12 @@ public class JWTUtils {
     @Value("${jwt-expiration-ms}")
     private int jwtExpirationMs;
 
+    public String generateJwtToken(MyAuthorDetails myAuthorDetails) {
+        return Jwts.builder()
+                .setSubject((myAuthorDetails.getUsername())) // just the user email
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
 }
