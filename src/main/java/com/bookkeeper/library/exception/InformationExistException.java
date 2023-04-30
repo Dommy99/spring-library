b284c2f5c8738b79d19674f98979b31caede56b8
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-30 14:04:24 -0500

    info exist ex doc

--- src/main/java/com/bookkeeper/library/exception/InformationExistException.java
+++ src/main/java/com/bookkeeper/library/exception/InformationExistException.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
 public class InformationExistException extends RuntimeException {
+    /**
+     * Constructor for InformationExistException
+     *
+     * @param message the error message for the exception
+     */
     public InformationExistException(String message) {
         super(message);
     }
