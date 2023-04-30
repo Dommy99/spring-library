a17241ebaedda7c5328a7e59aa331c33c7c9b73c
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-30 15:19:48 -0500

    my auth details doc

--- src/main/java/com/bookkeeper/library/security/MyAuthorDetails.java
+++ src/main/java/com/bookkeeper/library/security/MyAuthorDetails.java
@@ -14,42 +14,66 @@ public class MyAuthorDetails implements UserDetails {
     public MyAuthorDetails(Author author) {
         this.author = author;
     }
-
+    /**
+     * Returns an empty set of GrantedAuthority as the author doesn't have any authorities.
+     * @return Empty set of GrantedAuthority.
+     */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return new HashSet<>();
     }
-
+    /**
+     * Returns the author's password.
+     * @return Author's password.
+     */
     @Override
     public String getPassword() {
         return author.getPassword();
     }
-
+    /**
+     * Returns the author's email as the username.
+     * @return Author's email.
+     */
     @Override
     public String getUsername() {
         return author.getEmail();
     }
-
+    /**
+     * Returns true as the author's account never expires.
+     * @return True.
+     */
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
-
+    /**
+     * Returns true as the author's account is never locked.
+     * @return True.
+     */
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
-
+    /**
+     * Returns true as the author's credentials never expire.
+     * @return True.
+     */
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
-
+    /**
+     * Returns true as the author's account is always enabled.
+     * @return True.
+     */
     @Override
     public boolean isEnabled() {
         return true;
     }
-
+    /**
+     * Returns the author associated with this MyAuthorDetails.
+     * @return Author object.
+     */
     public Author getAuthor() {
         return author;
     }
