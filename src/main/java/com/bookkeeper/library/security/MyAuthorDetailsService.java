da2b0502016cfa5211f0580799eaeb0483c849b1
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-29 22:13:00 -0500

    autowired author service

--- src/main/java/com/bookkeeper/library/security/MyAuthorDetailsService.java
+++ src/main/java/com/bookkeeper/library/security/MyAuthorDetailsService.java
@@ -1,7 +1,16 @@
 package com.bookkeeper.library.security;
 
+
+import com.bookkeeper.library.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MyAuthorDetailsService {
+public class MyAuthorDetailsService implements AuthorDetailService {
+    private AuthorService authorService;
+
+    @Autowired
+    public void setAuthorService(AuthorService authorService){
+        this.authorService = authorService;
+    }
 }
