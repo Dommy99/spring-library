a4b13a7ae5c7fa79f2c3df37b6e4ec632d491aa8
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-29 21:33:49 -0500

    authorservice

--- src/main/java/com/bookkeeper/library/repository/AuthorRepository.java
+++ src/main/java/com/bookkeeper/library/repository/AuthorRepository.java
@@ -1,8 +1,14 @@
 package com.bookkeeper.library.repository;
 
+import com.bookkeeper.library.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuthorRepository  extends JpaRepository {
+public interface AuthorRepository  extends JpaRepository<Author, Long> {
+    // to register
+    boolean existsByEmailAddress(String userEmailAddress);
+
+    // to login
+    Author findUserByEmailAddress(String userEmailAddress);
 }
