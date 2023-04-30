861465bf2f77600df5918672d5900ac71729631d
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-29 23:52:29 -0500

    auth manager bean

--- src/main/java/com/bookkeeper/library/security/SecurityConfiguration.java
+++ src/main/java/com/bookkeeper/library/security/SecurityConfiguration.java
@@ -51,5 +51,10 @@ public class SecurityConfiguration {
         return http.build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
 
 }
