973754a9eff2de5174eea37ea8954689dacca8b6
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-30 15:04:22 -0500

    genre model doc

--- src/main/java/com/bookkeeper/library/model/Genre.java
+++ src/main/java/com/bookkeeper/library/model/Genre.java
@@ -23,25 +23,47 @@ public class Genre {
 
     public Genre() {
     }
-
+    /**
+     * Constructs a Genre object with the given id, genre name, and book list.
+     *
+     * @param id        the id of the genre.
+     * @param genreName the name of the genre.
+     * @param bookList  the list of books that belong to this genre.
+     */
     public Genre(Long id, String genreName, List<Book> bookList) {
         this.id = id;
         this.genreName = genreName;
         this.bookList = bookList;
     }
-
+    /**
+     * Returns the id of the genre.
+     *
+     * @return the id of the genre.
+     */
     public Long getId() {
         return id;
     }
-
+    /**
+     * Sets the id of the genre.
+     *
+     * @param id the id of the genre.
+     */
     public void setId(Long id) {
         this.id = id;
     }
-
+    /**
+     * Returns the name of the genre.
+     *
+     * @return the name of the genre.
+     */
     public String getGenreName() {
         return genreName;
     }
-
+    /**
+     * Sets the name of the genre.
+     *
+     * @param genreName the name of the genre.
+     */
     public void setGenreName(String genreName) {
         this.genreName = genreName;
     }
@@ -53,11 +75,19 @@ public class Genre {
                 ", genreName='" + genreName + '\'' +
                 '}';
     }
-
+    /**
+     * Returns the list of books that belong to this genre.
+     *
+     * @return the list of books that belong to this genre.
+     */
     public List<Book> getBookList() {
         return bookList;
     }
-
+    /**
+     * Sets the list of books that belong to this genre.
+     *
+     * @param bookList the list of books that belong to this genre.
+     */
     public void setBookList(List<Book> bookList) {
         this.bookList = bookList;
     }
