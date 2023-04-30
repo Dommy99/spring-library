b791358dad0526877f17ce38118cc549a47363bb
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-30 00:01:44 -0500

    login response

--- src/main/java/com/bookkeeper/library/controller/AuthorController.java
+++ src/main/java/com/bookkeeper/library/controller/AuthorController.java
@@ -3,6 +3,7 @@ package com.bookkeeper.library.controller;
 import com.bookkeeper.library.model.Author;
 import com.bookkeeper.library.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,4 +25,6 @@ public class AuthorController {
     public Author createUser(@RequestBody Author authorObject) {
         return authorService.createAuthor(authorObject);
     }
+
+
 }
