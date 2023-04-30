858b9d5421a10378900f1e04951ffdf078d44d49
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-30 14:00:43 -0500

    book controller doc

--- src/main/java/com/bookkeeper/library/controller/BookController.java
+++ src/main/java/com/bookkeeper/library/controller/BookController.java
@@ -27,26 +27,46 @@ public class BookController {
         this.bookService = bookService;
     }
 
-
+    /**
+     * Endpoint for creating a new book
+     *
+     * @param bookObject the book object to create
+     * @return the created book object
+     */
     // http://localhost:9092/api/books
     @PostMapping(path = "/books")
     public Book createBook(@RequestBody Book bookObject) {
 
         return bookService.createBook(bookObject);
     }
-
+    /**
+     * Endpoint for retrieving all books
+     *
+     * @return a list of all books
+     */
     // http://localhost:9092/api/books
     @GetMapping(path = "/books")
     public List<Book> getBooks() {
         return bookService.getBooks();
     }
-
+    /**
+     * Endpoint for retrieving a single book by ID
+     *
+     * @param bookId the ID of the book to retrieve
+     * @return the optional book object with the given ID
+     */
     // http://localhost:9092/api/books/{bookId}
     @GetMapping(path = "/books/{bookId}")
     public Optional<Book> getBook(@PathVariable Long bookId) {
         return bookService.getBook(bookId);
     }
-
+    /**
+     * Endpoint for updating a single book by ID
+     *
+     * @param bookId the ID of the book to update
+     * @param bookObject the updated book object
+     * @return the updated book object
+     */
     // http://localhost:9092/api/books/{bookId}
     @PutMapping(path ="/books/{bookId}")
     public Book updateBook(@PathVariable(value = "bookId") Long bookId, @RequestBody Book bookObject) {
@@ -54,7 +74,12 @@ public class BookController {
         return bookService.updateBook(bookId, bookObject);
     }
 
-
+    /**
+     * Endpoint for deleting a single book by ID
+     *
+     * @param bookId the ID of the book to delete
+     * @return the optional book object that was deleted
+     */
     // http://localhost:9092/api/books/{bookId}
     @DeleteMapping(path ="/books/{bookId}")
     public Optional<Book> deleteBook(@PathVariable(value = "bookId") Long bookId) {
