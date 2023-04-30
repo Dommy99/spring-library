ea2a16dc74feaadb4f12272b878a48fff141c6ae
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-30 01:21:51 -0500

    author service doc comeleted

--- src/main/java/com/bookkeeper/library/service/AuthorService.java
+++ src/main/java/com/bookkeeper/library/service/AuthorService.java
@@ -65,22 +65,34 @@ public class AuthorService {
 
 
     /**
+     * Finds an author by their email address.
      *
-     * @param email
-     * @return
+     * @param email the email address of the author
+     * @return the found Author object or null if not found
      */
     public Author findAuthorByEmail(String email) {
         return authorRepository.findAuthorByEmail(email);
     }
 
+    /**
+     * Authenticates a user and generates a JWT token if the authentication is successful.
+     *
+     * @param loginRequest the LoginRequest object containing the user's email and password
+     * @return a ResponseEntity object with either the generated JWT token or an error message
+     */
     public ResponseEntity<?> loginUser(LoginRequest loginRequest) {
         try {
+            // Authenticate the user with the provided email and password
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+            // Set the authenticated user in the security context
             SecurityContextHolder.getContext().setAuthentication(authentication);
+            // Get the authenticated user details
             myAuthorDetails = (MyAuthorDetails) authentication.getPrincipal();
 
+            // Generate a JWT token for the authenticated user
             final String JWT = jwtUtils.generateJwtToken(myAuthorDetails);
+            // Return the generated JWT token in the response
             return ResponseEntity.ok(new LoginResponse(JWT));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new LoginResponse("Error: username or password is incorrect"));
