fbe2a3947452712b1d2119437983d5809196eb26
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-29 21:36:44 -0500

    security

--- src/main/java/com/bookkeeper/library/service/AuthorService.java
+++ src/main/java/com/bookkeeper/library/service/AuthorService.java
@@ -1,7 +1,17 @@
 package com.bookkeeper.library.service;
 
+import com.bookkeeper.library.repository.AuthorRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthorService {
+
+    private final AuthorRepository authorRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthorService(AuthorRepository authorRepository, PasswordEncoder passwordEncoder) {
+        this.authorRepository = authorRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 }
