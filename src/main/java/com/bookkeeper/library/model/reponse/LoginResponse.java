efc72da5a09108cbe4573c2456235d5a14364cbe
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-30 14:18:41 -0500

    login res doc

--- src/main/java/com/bookkeeper/library/model/reponse/LoginResponse.java
+++ src/main/java/com/bookkeeper/library/model/reponse/LoginResponse.java
@@ -2,15 +2,27 @@ package com.bookkeeper.library.model.reponse;
 
 public class LoginResponse {
     private String message;
-
+    /**
+     * Constructor for LoginResponse
+     *
+     * @param message the message to set for the response object
+     */
     public LoginResponse(String message) {
         this.message = message;
     }
-
+    /**
+     * Getter for the message field
+     *
+     * @return the message field value
+     */
     public String getMessage() {
         return message;
     }
-
+    /**
+     * Setter for the message field
+     *
+     * @param message the message to set for the response object
+     */
     public void setMessage(String message) {
         this.message = message;
     }
