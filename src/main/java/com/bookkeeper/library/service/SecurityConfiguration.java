d2e23ce0742573b74e4c13d2048e385bed080959
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-29 23:35:39 -0500

    jwtReqFilter added

--- src/main/java/com/bookkeeper/library/service/SecurityConfiguration.java
+++ src/main/java/com/bookkeeper/library/service/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -17,5 +18,15 @@ public class SecurityConfiguration {
         this.myAuthorDetailsService = myAuthorDetailsService;
     }
 
+    @Bean
+    public JwtRequestFilter authJwtRequestFilter() {
+        return new JwtRequestFilter();
+    }
+
+    // register user
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
