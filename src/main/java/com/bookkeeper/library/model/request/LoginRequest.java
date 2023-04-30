9a6ee1184a857a945142b951673f78497d2e89f5
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-30 14:21:39 -0500

    login req doc

--- src/main/java/com/bookkeeper/library/model/request/LoginRequest.java
+++ src/main/java/com/bookkeeper/library/model/request/LoginRequest.java
@@ -3,11 +3,19 @@ package com.bookkeeper.library.model.request;
 public class LoginRequest {
     private String email;
     private String password;
-
+    /**
+     * Getter for the email field
+     *
+     * @return the email field value
+     */
     public String getEmail() {
         return email;
     }
-
+    /**
+     * Getter for the password field
+     *
+     * @return the password field value
+     */
     public String getPassword() {
         return password;
     }
