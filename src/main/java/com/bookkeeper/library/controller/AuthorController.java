e96d4f583aa05a711209caee3e3b8809fdc2b5b7
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-04-29 23:15:03 -0500

    register author

--- src/main/java/com/bookkeeper/library/controller/AuthorController.java
+++ src/main/java/com/bookkeeper/library/controller/AuthorController.java
@@ -1,6 +1,10 @@
 package com.bookkeeper.library.controller;
 
+import com.bookkeeper.library.model.Author;
 import com.bookkeeper.library.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +14,14 @@ public class AuthorController {
 
     private AuthorService authorService;
 
+    @Autowired
+    public void setAuthorService(AuthorService authorService) {
+        this.authorService = authorService;
+    }
 
+    // http://localhost:9092/auth/authors/register/
+    @PostMapping("/register/")
+    public Author createUser(@RequestBody Author authorObject) {
+        return authorService.createAuthor(authorObject);
+    }
 }
